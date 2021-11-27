3eb3dbf02c33d05e0994efd1c9893d578fdd29da
Stephen Webber <offgridauthor@gmail.com>
2021-11-26 18:20:55 -0800

    add axios

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -3,20 +3,23 @@ import axios from 'axios';
 
 const Search = () => {
     const [term, setTerm] = useState('');
+    const [results, setResults] = useState([]);
 
     useEffect(() => {
-        (async () => {
-            await axios.get('https://en.wikipedia.org/w/api.php', {
+        const search = async () => {
+            const {data} = await axios.get('https://en.wikipedia.org/w/api.php', {
                 params: {
                     action: 'query',
                     list: 'search',
                     origin: '*',
                     format: 'json',
                     srsearch: term,
-                }
+                },
             })
-        })()
+            setResults(data.query.search)
 
+        };
+        search();
     }, [term])
 
     return (
