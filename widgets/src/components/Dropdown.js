659981f77e2b0d72b12c4ae8011fc8d615345901
Stephen Webber <offgridauthor@gmail.com>
2021-11-28 15:44:58 -0800

    init toggle

--- widgets/src/components/Dropdown.js
+++ widgets/src/components/Dropdown.js
@@ -1,60 +1,68 @@
-import React, {useState, useEffect, useRef} from 'react';
+import React, { useState, useEffect, useRef } from "react";
 
-const Dropdown = ({options, selected, onSelectedChange}) => {
-
+const Dropdown = ({ options, selected, onSelectedChange }) => {
     const [open, setOpen] = useState(false);
     const ref = useRef();
 
     useEffect(() => {
-        document.body.addEventListener('click', (event) => {
-            if(ref.current.contains(event.target)){
-                return;// if click happens inside component
+        const onBodyClick = (event) => {
+            if (ref.current.contains(event.target)) {
+                return;
             }
-                setOpen(false); // body clicked
-            }, {capture: true}
-        )
-    }, [])//empty array = only run once
+            setOpen(false);
+        };
+        document.body.addEventListener("click", onBodyClick, { capture: true });
+
+        return () => {
+            document.body.removeEventListener("click", onBodyClick, {
+                capture: true,
+            });
+        };
+    }, []);
+
+    useEffect(() => {
+        document.body.addEventListener(
+            "click",
+            () => {
+                setOpen(false);
+            },
+            { capture: true }
+        );
+    }, []);
 
     const renderedOptions = options.map((option) => {
         if (option.value === selected.value) {
-            return (
-                <div key={option.value} className="item"
-                     onClick={() => onSelectedChange(option)}
-                >
-                    <i className="ae flag"></i>
-                    {option.label}
-                </div>
-            )
+            return null;
         }
 
-// item clicked
         return (
-            <div key={option.value} className="item"
-                 onClick={() => onSelectedChange(option)}
+            <div
+                key={option.value}
+                className="item"
+                onClick={() => onSelectedChange(option)}
             >
                 {option.label}
             </div>
         );
-    })
+    });
 
-//ref.current
     return (
-
         <div ref={ref} className="ui form">
             <div className="field">
-                <label className="label">SELECT A COLOR</label>
-                <div onClick={() => setOpen(!open)}
-                     className={`ui selection dropdown ${open ? 'visible active' : ''}`}
+                <label className="label">Select a Color</label>
+                <div
+                    onClick={() => setOpen(!open)}
+                    className={`ui selection dropdown ${open ? "visible active" : ""}`}
                 >
                     <i className="dropdown icon"></i>
                     <div className="text">{selected.label}</div>
-                    <div className={`menu ${open ? 'visible transition' : ''} `}>
+                    <div className={`menu ${open ? "visible transition" : ""}`}>
                         {renderedOptions}
                     </div>
                 </div>
             </div>
         </div>
-    ) // dropdown click
-}
+    );
+};
 
-export default Dropdown
+export default Dropdown;
