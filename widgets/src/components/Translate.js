50a08397b90b163aae8fce412471c4c291ce3c66
Stephen Webber <offgridauthor@gmail.com>
2021-11-28 16:15:57 -0800

    init translate

--- widgets/src/components/Translate.js
+++ widgets/src/components/Translate.js
@@ -17,10 +17,11 @@ const options = [
 const Translate = () => {
 
     const [language, setLanguage] = useState(options[0])
-
+    const [text, setText] = useState('');
 
     return (
         <div>
+            <input value={text} onChange={(e) => setText(e.target.value)}/>
             <Dropdown options={options}
                       selected={language}
                       onSelectedChange={setLanguage}
