3f99fa2c1f329a10429ae1b6b4ae64ca5e39d686
Stephen Webber <offgridauthor@gmail.com>
2021-11-29 20:29:37 -0800

    hooks with video

--- videos-hooks/src/components/App.js
+++ videos-hooks/src/components/App.js
@@ -24,9 +24,9 @@ const App = () => {
     setVideos(response.data.items)
     setSelectedVideo(response.data.items[0])
 
-    const onVideoSelect = (video) => {
-      setSelectedVideo(video)
-    }
+    // const onVideoSelect = (video) => {
+    //   setSelectedVideo(video)
+    // }
 
     return (
         <div className="ui container">
@@ -38,7 +38,7 @@ const App = () => {
               </div>
               <div className="five wide column">
                 <VideoList
-                    onVideoSelect={onVideoSelect}
+                    onVideoSelect={setSelectedVideo}
                     videos={videos}
                 />
               </div>
