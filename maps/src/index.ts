c184ccc6e8ae6f01d2ba04d926ea7c327114c0c9
Stephen Webber <offgridauthor@gmail.com>
2021-12-06 11:41:26 -0800

    ts experiment

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -6,4 +6,10 @@ const company = new Company();
 
 
 
-new google.maps.Map(document.getElementById('map'))
+new google.maps.Map(document.getElementById('map'), {
+    zoom: 1,
+    center: {
+        lat: 0,
+        lng: 0
+    }
+})
