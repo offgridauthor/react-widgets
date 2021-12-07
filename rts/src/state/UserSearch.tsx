f2def515bea95b566284745bdc0bb995633c0485
Stephen Webber <offgridauthor@gmail.com>
2021-12-07 10:09:20 -0800

    ts experiment

---
+++ rts/src/state/UserSearch.tsx
@@ -0,0 +1,12 @@
+import {useState} from "react";
+
+
+const UserSearch: React.FC = () => {
+    return (
+        <div>
+            user serch
+        </div>
+    )
+}
+
+export default UserSearch
