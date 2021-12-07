c752e21d932cf0d2a2c66df1703e056c7a0d854c
Stephen Webber <offgridauthor@gmail.com>
2021-12-07 10:14:12 -0800

    ts experiment

--- rts/src/state/UserSearch.tsx
+++ rts/src/state/UserSearch.tsx
@@ -1,10 +1,29 @@
 import {useState} from "react";
 
+const users = [
+    {name: 'Sarah', age: 20},
+    {name: 'God the Father', age: 16},
+    {name: 'Heruziamchel', age: 2000},
+
+]
 
 const UserSearch: React.FC = () => {
+
+    const [name, setName] = useState('')
+
+    const onclick = () => {
+        const foundUser = users.find((user)=> {
+            return user.name === name
+        })
+        console.log(foundUser)
+
+    }
+
     return (
         <div>
             user serch
+            <input value={name} onChange={e => setName(e.target.value)}/>
+            <button onClick={onclick}>Find user</button>
         </div>
     )
 }
