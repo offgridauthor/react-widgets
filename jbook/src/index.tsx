18f101d2c54e9d63e38329cf94a9cbf8cb1fca2f
Stephen Webber <offgridauthor@gmail.com>
2021-12-08 10:26:03 -0800

    transpiler functional

--- jbook/src/index.tsx
+++ jbook/src/index.tsx
@@ -19,10 +19,17 @@ const App = () => {
         startService()
     }, [])
 
-    const onClick = () => {
-        if(!ref.current){ 
+    const onClick = async () => {
+        if (!ref.current) {
             return
         }
+
+        const result = await ref.current.transform(input, {
+            loader: 'jsx',
+            target: 'es2015',
+        })
+
+        setCode(result.code)
     }
 
     return (
