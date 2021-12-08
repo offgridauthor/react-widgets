b2ec61c52f0461630e616dec9c9fccefe52f27ee
Stephen Webber <offgridauthor@gmail.com>
2021-12-08 14:10:24 -0800

    plugin for esbuild using unpkg complete

--- jbook/src/index.tsx
+++ jbook/src/index.tsx
@@ -1,59 +1,56 @@
-import * as esbuild from 'esbuild-wasm'
-import {useState, useEffect, useRef} from 'react'
-import ReactDOM from 'react-dom'
-import {unpkgPathPlugin} from "./plugins/unpkg-path-plugin";
+import * as esbuild from 'esbuild-wasm';
+import { useState, useEffect, useRef } from 'react';
+import ReactDOM from 'react-dom';
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 
 const App = () => {
-    const ref = useRef<any>()
-    const [input, setInput] = useState('')
-    const [code, setCode] = useState('')
+  const ref = useRef<any>();
+  const [input, setInput] = useState('');
+  const [code, setCode] = useState('');
 
-    const startService = async () => {
-        ref.current = await esbuild.startService({
-            worker: true,
-            wasmURL: '/esbuild.wasm'
-        })
-        // console.log(service)
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: '/esbuild.wasm',
+    });
+  };
+  useEffect(() => {
+    startService();
+  }, []);
+
+  const onClick = async () => {
+    if (!ref.current) {
+      return;
     }
 
-    useEffect(() => {
-        startService()
-    }, [])
+    const result = await ref.current.build({
+      entryPoints: ['index.js'],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin()],
+      define: {
+        'process.env.NODE_ENV': '"production"',
+        global: 'window',
+      },
+    });
 
-    const onClick = async () => {
-        if (!ref.current) {
-            return
-        }
+    // console.log(result);
 
-        // const result = await ref.current.transform(input, {
-        //     loader: 'jsx',
-        //     target: 'es2015',
-        // }) // renders code without using unpkg plugin just to test basic transpiling (doesn't handle imports)
-        // setCode(result.code) // pre unpkg
+    setCode(result.outputFiles[0].text);
+  };
 
-        const result = await ref.current.build({
-            entryPoints: ['index.js'],
-            bundle: true,
-            write: false,
-            plugins: [unpkgPathPlugin()]
-        })
+  return (
+    <div>
+      <textarea
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      ></textarea>
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
+    </div>
+  );
+};
 
-        setCode(result.outputFiles[0].text) // output of bundling process using plugin
-
-    }
-
-    return (
-        <div>
-            <textarea value={input} onChange={event => setInput(event.target.value)}/>
-            <div>
-                <button onClick={onClick}>Submit</button>
-            </div>
-            <pre>{code}</pre>
-        </div>
-    )
-}
-
-ReactDOM.render(
-    <App/>,
-    document.querySelector('#root')
-)
+ReactDOM.render(<App />, document.querySelector('#root'));
--- jbook/src/plugins/unpkg-path-plugin.ts
+++ jbook/src/plugins/unpkg-path-plugin.ts
@@ -1,38 +1,52 @@
 import * as esbuild from 'esbuild-wasm';
-import axios from "axios";
+import axios from 'axios';
 
 export const unpkgPathPlugin = () => {
-    return {
-        name: 'unpkg-path-plugin',
-        setup(build: esbuild.PluginBuild) {
-            build.onResolve({ filter: /.*/ }, async (args: any) => {
-                console.log('onResolve', args);
-                if(args.path === 'index.js') {
-                    return { path: args.path, namespace: 'a' };
-                } else if(args.path==='tiny-test-pkg'){
-                    return {path: 'https://unpkg.com/tiny-test-pkg@1.0.0/index.js', namespace: 'a'}
-                }
-            });
+  return {
+    name: 'unpkg-path-plugin',
+    setup(build: esbuild.PluginBuild) {
+      build.onResolve({ filter: /.*/ }, async (args: any) => {
+        console.log('onResolve', args);
+        if (args.path === 'index.js') {
+          return { path: args.path, namespace: 'a' };
+        }
 
-            build.onLoad({ filter: /.*/ }, async (args: any) => {
-                console.log('onLoad', args);
+        if (args.path.includes('./') || args.path.includes('../')) {
+          return {
+            namespace: 'a',
+            path: new URL(
+              args.path,
+              'https://unpkg.com' + args.resolveDir + '/'
+            ).href,
+          };
+        }
 
-                if (args.path === 'index.js') {
-                    return {
-                        loader: 'jsx',
-                        contents: `
-              import message from 'tiny-test-pkg';
-              console.log(message);
+        return {
+          namespace: 'a',
+          path: `https://unpkg.com/${args.path}`,
+        };
+      });
+
+      build.onLoad({ filter: /.*/ }, async (args: any) => {
+        console.log('onLoad', args);
+
+        if (args.path === 'index.js') {
+          return {
+            loader: 'jsx',
+            contents: `
+              import React, { useState } from 'react';
+              console.log(React, useState);
             `,
-                    };
-                }
+          };
+        }
 
-                const {data} = await axios.get(args.path)
-                return {
-                    loader: 'jsx',
-                    contents: data
-                }
-            });
-        },
-    };
+        const { data, request } = await axios.get(args.path);
+        return {
+          loader: 'jsx',
+          contents: data,
+          resolveDir: new URL('./', request.responseURL).pathname,
+        };
+      });
+    },
+  };
 };
