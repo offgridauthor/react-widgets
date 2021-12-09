3b317bf4c5fce21dc6dde58f19667d82e9d04862
Stephen Webber <offgridauthor@gmail.com>
2021-12-09 14:58:51 -0800

    md editor toggling view edit

--- jbook/src/components/text-editor.tsx
+++ jbook/src/components/text-editor.tsx
@@ -1,37 +1,42 @@
-import MDEditor from '@uiw/react-md-editor'
-import {useState, useEffect} from "react";
+import { useState, useEffect, useRef } from 'react';
+import MDEditor from '@uiw/react-md-editor';
 
 const TextEditor: React.FC = () => {
-    const [editing, setEditing] = useState(false)
+  const ref = useRef<HTMLDivElement | null>(null);
+  const [editing, setEditing] = useState(false);
 
+  useEffect(() => {
+    const listener = (event: MouseEvent) => {
+      if (
+        ref.current &&
+        event.target &&
+        ref.current.contains(event.target as Node)
+      ) {
+        return;
+      }
 
-    useEffect(() => {
-        const listener = () => {
-            setEditing(false)
-        }
+      setEditing(false);
+    };
+    document.addEventListener('click', listener, { capture: true });
 
-        document.addEventListener('click', listener, {capture: true})
+    return () => {
+      document.removeEventListener('click', listener, { capture: true });
+    };
+  }, []);
 
-        return () => {
-            document.removeEventListener('click', listener, {capture:true})
-        }
-    }, [])
-
-
-
-    if(editing){
-        return(
-            <div>
-                <MDEditor/>
-            </div>
-        )
-    }
-
+  if (editing) {
     return (
-        <div onClick={() => setEditing(true)}>
-            <MDEditor.Markdown source={'# Header'}/>
-        </div>
-    )
-}
+      <div ref={ref}>
+        <MDEditor />
+      </div>
+    );
+  }
 
-export default TextEditor
+  return (
+    <div onClick={() => setEditing(true)}>
+      <MDEditor.Markdown source={'# Header'} />
+    </div>
+  );
+};
+
+export default TextEditor;
