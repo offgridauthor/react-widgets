3e7a84f014c1891cbb555623802a375704f8f3f7
Stephen Webber <offgridauthor@gmail.com>
2021-12-09 07:56:28 -0800

    monaco editor props

--- jbook/src/components/code-editor.tsx
+++ jbook/src/components/code-editor.tsx
@@ -1,7 +1,33 @@
 import MonacoEditor from '@monaco-editor/react';
 
-const CodeEditor = () => {
-  return <MonacoEditor height="500px" />;
+interface CodeEditorProps {
+    initialValue: string;
+    onChange(value:string): void;
+}
+
+const CodeEditor: React.FC<CodeEditorProps> = ({onChange, initialValue}) => {
+    const onEditorDidMount = (getValue: () => string) => {
+        
+    }
+    return <MonacoEditor
+        editorDidMount={onEditorDidMount}
+        value={initialValue}
+        language="javascript"
+        theme="dark"
+        height="50vh"
+        width="50vw"
+        options={{
+            wordWrap: 'on',
+            minimap: {enabled: false},
+            showUnused: false,
+            folding: false,
+            lineNumbersMinChars: 2,
+            fontSize: 16,
+            scrollBeyondLastLine: false,
+            automaticLayout: true,
+        }}
+
+    />;
 };
 
 export default CodeEditor;
