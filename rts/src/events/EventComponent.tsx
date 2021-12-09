6ef275bc1d24eb71b1eb0cfaa6396dfb974848bc
Stephen Webber <offgridauthor@gmail.com>
2021-12-09 07:03:28 -0800

    iframe sandbox

--- rts/src/events/EventComponent.tsx
+++ rts/src/events/EventComponent.tsx
@@ -1,19 +1,20 @@
 const EventComponent: React.FC = () => {
-    const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        console.log(event)
-    }
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(event);
+  };
 
+  const onDragStart = (event: React.DragEvent<HTMLDivElement>) => {
+    console.log(event);
+  };
 
-    const ondragstart = (event: React.DragEvent<HTMLDivElement>) => {
-        
-    }
+  return (
+    <div>
+      <input onChange={onChange} />
+      <div draggable onDragStart={onDragStart}>
+        Drag Me!
+      </div>
+    </div>
+  );
+};
 
-    return (
-        <div>
-            <input onChange={onChange}/>
-            <div draggable onDragStart={ondragstart}>DRAG ME</div>
-        </div>
-    )
-}
-
-export default EventComponent
+export default EventComponent;
--- rts/src/refs/UserSearch.tsx
+++ rts/src/refs/UserSearch.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 
 const users = [
   { name: 'Sarah', age: 20 },
@@ -7,8 +7,17 @@ const users = [
 ];
 
 const UserSearch: React.FC = () => {
+  const inputRef = useRef<HTMLInputElement | null>(null);
   const [name, setName] = useState('');
   const [user, setUser] = useState<{ name: string; age: number } | undefined>();
+
+  useEffect(() => {
+    if (!inputRef.current) {
+      return;
+    }
+
+    inputRef.current.focus();
+  }, []);
 
   const onClick = () => {
     const foundUser = users.find((user) => {
@@ -21,7 +30,11 @@ const UserSearch: React.FC = () => {
   return (
     <div>
       User Search
-      <input value={name} onChange={(e) => setName(e.target.value)} />
+      <input
+        ref={inputRef}
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
       <button onClick={onClick}>Find User</button>
       <div>
         {user && user.name}
