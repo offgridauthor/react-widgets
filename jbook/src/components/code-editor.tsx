b6dd8352012a2c966b87a4826b021099c64e8d31
Stephen Webber <offgridauthor@gmail.com>
2021-12-09 08:20:22 -0800

    bulma css with format button

--- jbook/src/components/code-editor.tsx
+++ jbook/src/components/code-editor.tsx
@@ -1,6 +1,8 @@
-import MonacoEditor, {EditorDidMount} from '@monaco-editor/react';
+import './code-editor.css'
+import MonacoEditor, {EditorDidMount} from '@monaco-editor/react'
 import prettier from 'prettier'
 import parser from 'prettier/parser-babel'
+import {useRef} from "react"
 
 interface CodeEditorProps {
     initialValue: string;
@@ -9,31 +11,53 @@ interface CodeEditorProps {
 }
 
 const CodeEditor: React.FC<CodeEditorProps> = ({onChange, initialValue}) => {
+    const editorRef = useRef<any>()
+
     const onEditorDidMount: EditorDidMount = (getValue: () => string, monacoEditor: any) => {
+        editorRef.current = monacoEditor
         monacoEditor.onDidChangeModelContent(() => {
             onChange(getValue())
         })
         monacoEditor.getModel()?.updateOptions({tabSize: 2})
     }
-    return <MonacoEditor
-        editorDidMount={onEditorDidMount}
-        value={initialValue}
-        language="javascript"
-        theme="dark"
-        height="50vh"
-        width="50vw"
-        options={{
-            wordWrap: 'on',
-            minimap: {enabled: false},
-            showUnused: false,
-            folding: false,
-            lineNumbersMinChars: 2,
-            fontSize: 16,
-            scrollBeyondLastLine: false,
-            automaticLayout: true,
-        }}
 
-    />;
+    const onFormatClick = () => {
+        const unformatted = editorRef.current.getModel().getValue()
+        const formatted = prettier.format(unformatted, {
+            parser: 'babel',
+            plugins: [parser],
+            useTabs:false,
+            semi: true,
+            singleQuote:true,
+        }).replace(/\n$/, '')
+        editorRef.current.setValue(formatted)
+    }
+
+    return (
+        <div className="editor-wrapper">
+            <button className="button button-format is-primary is-small" onClick={onFormatClick}>Format</button>
+            <MonacoEditor
+                editorDidMount={onEditorDidMount}
+                value={initialValue}
+                language="javascript"
+                theme="dark"
+                height="50vh"
+                width="50vw"
+                options={{
+                    wordWrap: 'on',
+                    minimap: {enabled: false},
+                    showUnused: false,
+                    folding: false,
+                    lineNumbersMinChars: 2,
+                    fontSize: 16,
+                    scrollBeyondLastLine: false,
+                    automaticLayout: true,
+                }}
+
+            />
+        </div>)
+
+
 };
 
 export default CodeEditor;
